ea3d5237599f0db90b8be1ce11b84a12aa80617d
Michelle Chou <chou.m.rdh@gmail.com>
2021-06-06 08:42:52 -0700

    account page

--- src/pages/Accountpage.js
+++ src/pages/Accountpage.js
@@ -1,33 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import HomeIcon from '@material-ui/icons/Home';
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import IconButton from "@material-ui/core/IconButton";
-import DirectionsIcon from "@material-ui/icons/Directions";
-import clsx from 'clsx';
-import Input from '@material-ui/core/Input';
-import FilledInput from '@material-ui/core/FilledInput';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import BorderColorIcon from '@material-ui/icons/BorderColor';
+import clsx from "clsx";
+import FormControl from "@material-ui/core/FormControl";
+
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="">
+      <Link color="inherit" href="/">
         RoomBud
       </Link>{" "}
       {new Date().getFullYear()}
@@ -45,10 +36,10 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.primary.main,
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: "100%",
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -56,36 +47,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Account({firstname, setFirstname, lastname, setLastname}) {
+export default function Account({
+  firstname,
+  setFirstname,
+  lastname,
+  setLastname,
+}) {
   const accountStorage = window.localStorage;
-  console.log(firstname, lastname)
 
   const classes = useStyles();
-  const [values, setValues] = React.useState({
-    amount: '',
-    password: '',
-    weight: '',
-    weightRange: '',
-    showPassword: false,
-  });
-
-  
-
-  const updateBtn =()=>{
+  const updateBtn = () => {
     accountStorage.setItem("firstName", firstname);
-    accountStorage.setItem("lastName", lastname)
-
-  };
-
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword });
-  };
-
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
+    accountStorage.setItem("lastName", lastname);
   };
 
   return (
@@ -93,93 +66,65 @@ export default function Account({firstname, setFirstname, lastname, setLastname}
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
+          <AccountCircleIcon />
         </Avatar>
         <Typography component="h1" variant="h5" className="account">
           Account Profile
         </Typography>
         <form className={classes.form} noValidate>
-        <FormControl
+          <FormControl
             fullWidth
             className={clsx(classes.margin, classes.textField)}
             variant="outlined"
           >
-            {/* <InputLabel 
-                htmlFor="outlined-adornment-password" 
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                id="firstName"
-                label="First Name"
-                name="firstName"
-            >
-              First Name
-            </InputLabel>
-            <Input 
-                value={username}
-                onChange={(event) => {setUsername(event.target.value)}}
-                // onChange={handleChange}
-                // name="textmask"
-                // id="formatted-text-mask-input"
-            /> */}
-            <TextField 
-              label="First Name"
+            <TextField
               variant="outlined"
+              margin="normal"
+              name="FirstName"
+              label="First Name"
+              type="firstName"
               value={firstname}
-              onChange={(event) => {setFirstname(event.target.value)}}
+              id="firstName"
+              onChange={(event) => {
+                setFirstname(event.target.value);
+              }}
             />
-           
-           <TextField
-            variant="outlined"
-            margin="normal"
-            name="LastName"
-            label="Last Name"
-            type="lastName"
-            id="lastName"
-            value={lastname}
-            onChange={(event) => {setLastname(event.target.value)}}
-            // autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={updateBtn}
-          >
+
+            <TextField
+              variant="outlined"
+              margin="normal"
+              name="LastName"
+              label="Last Name"
+              type="lastName"
+              id="lastName"
+              value={lastname}
+              onChange={(event) => {
+                setLastname(event.target.value);
+              }}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={updateBtn}
+            >
               Update
-          </Button>
-            {/* <OutlinedInput
-              id="outlined-adornment-password"
-              type={values.showPassword ? "text" : "password"}
-              value={values.password}
-              onChange={handleChange("password")}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                   <BorderColorIcon/>
-                  </IconButton>
-                </InputAdornment>
-              }
-              labelWidth={70}
-            /> */}
+            </Button>
           </FormControl>
         </form>
         <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            <a className="loginLink" href="/home">Home</a>
-          </Button>
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        ><HomeIcon/>
+          <a className="loginLink" href="/home">
+            Home
+          </a>
+        </Button>
       </div>
       <Box mt={8}>
         <Copyright />
