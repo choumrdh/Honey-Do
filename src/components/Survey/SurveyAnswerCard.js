fce491537494e4056eafae50cf6f3aa34aff4d74
Michelle Chou <chou.m.rdh@gmail.com>
2021-06-21 22:52:14 -0700

    connect answer key

--- src/components/Survey/SurveyAnswerCard.js
+++ src/components/Survey/SurveyAnswerCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Card } from "@material-ui/core";
+import { Card, Grid } from "@material-ui/core";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 
@@ -8,9 +8,9 @@ const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(2),
     alignItems: "center",
-    minWidth: 350,
+    minWidth: 360,
     maxWidth:600,
-    display: 'flex'
+   
   },
   Card: {
     margin: theme.spacing(3),
@@ -19,35 +19,36 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const SurveyAnswerCard = ({ surveyAnswers, getDefault }) => {
-  console.log("answer card", surveyAnswers[0][0]);
-  console.log("default", getDefault.questions[4].label);
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       {surveyAnswers.map((answer) => {
         return (
-          <Card className={classes.Card} variant="outlined">
+          <Grid xs={12} sm={12} lg={12}>
+            <Card className={classes.Card} variant="outlined">
             <Typography gutterBottom variant="h5" component="h2">Title:{getDefault.title}</Typography>
             
             <Typography variant="h6" gutterBottom>
-              {getDefault.questions[0].label}: {answer[0]}
+              {getDefault.questions[0].label}: {answer[getDefault.questions[0].id]}
             </Typography>
             <Typography variant="h6">
-              {getDefault.questions[1].label}: {answer[1]}
+              {getDefault.questions[1].label}: {answer[getDefault.questions[1].id]}
               </Typography>
             <CardContent>
               <Typography variant="subtitle1">
-                {getDefault.questions[2].label}: {answer[2]}
+                {getDefault.questions[2].label}: {answer[getDefault.questions[2].id]}
                 </Typography>
               <Typography variant="subtitle1" display="block">
-                {getDefault.questions[3].label}: {answer[3]}
+                {getDefault.questions[3].label}: {answer[getDefault.questions[3].id]}
               </Typography>
               <Typography variant="subtitle1" display="block">
-                {getDefault.questions[4].label}: {answer[4]}
+                {getDefault.questions[4].label}: {answer[getDefault.questions[4].id]}
               </Typography>
             </CardContent>
           </Card>
+          </Grid>
+          
         );
       })}
     </div>
